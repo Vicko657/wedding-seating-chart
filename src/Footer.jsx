3bc3e78402933aff0035d6ef1b5b6e391d0f40d4
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-09-19 13:29:41 +0100

    feat: add signature to footer and update structure and styling

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,9 +4,21 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <>
-      <footer className="d-flex App-header row">
-        <img className="logo mb-2" src="/imgs/centred_s&s.svg" alt="logo" />
-        <h2>WEDNESDAY 6TH AUGUST 2025</h2>
+      <footer className="d-flex row mb-5 ">
+        <div className="col-12">
+          <img
+            className="logo d-block mx-auto "
+            src="/imgs/centred_s&s.svg"
+            alt="logo"
+          />
+          <h2>WEDNESDAY 6TH AUGUST 2025</h2>
+        </div>
+        <div className="signature col-12 p-2 text-center">
+          💻 Designed & Developed by
+          <a className="ms-1" href="https://www.victoriaolusegun.com/">
+            Victoria
+          </a>
+        </div>
       </footer>
     </>
   );
