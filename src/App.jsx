a98984ca8e800525ade666448f342efa9ab3d7d5
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-09-19 13:34:51 +0100

    fix: update component name and mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import "./App.css";
 import MainBody from "./MainBody";
-import Header from "./Header";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="container-fluid p-0 m-0 App">
       <div className="row p-0 m-0">
-        <div className="left-column col-lg-6  d-flex flex-column overflow-auto vh-100">
-          <div className="align-content-center vh-100">
-            <MainBody />
-          </div>
-          <Header />
+        <div className="d-flex flex-column align-items-center vh-100">
+          <MainBody />
+          <Footer />
         </div>
       </div>
     </div>
